3886d8822d49c9b3e917b07b72fa42b65d3b805d
Iceman <okamura@qoncept.co.jp>
2024-12-17 12:25:03 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "CallableKitCodegen",
+    platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "codegen", targets: ["Codegen"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
+        .package(url: "https://github.com/omochi/CodableToTypeScript.git", from: "3.0.1"),
+        .package(url: "https://github.com/omochi/SwiftTypeReader.git", from: "3.1.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Codegen",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "CodegenImpl",
+            ]
+        ),
+        .target(
+            name: "CodegenImpl",
+            dependencies: [
+                "CodableToTypeScript",
+                "SwiftTypeReader",
+            ],
+            swiftSettings: [
+                .enableUpcomingFeature("BareSlashRegexLiterals"),
+            ]
+        ),
+    ]
+)
